d6bed702fb652dbe282a473051f724373712887c
pkbmisesw <pkbmisesw@gmail.com>
2024-06-04 20:04:04 +0800

    tesa

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ if(isset($_POST['login'])) {
 <head>
 
     <meta charset="utf-8" />
-    <title>SI-Lahan Kawan</title>
+    <title>SI-Lahan Kawana</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesdesign" name="author" />
