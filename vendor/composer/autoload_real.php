50e2007178406c25aee8b5563eb0ce6f27fd5ad1
rezaawp <rezaaawp@gmail.com>
2024-06-13 19:47:13 +0700

    add library eloquent

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitac06b0f35f4adc2670ec6ec8721fa178
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitac06b0f35f4adc2670ec6ec8721fa178::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
